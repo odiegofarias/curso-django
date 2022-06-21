07958b6ce414328971a4cb68b497df29106d4571
Diego Farias <diego@exemplo.com>
2022-06-21 20:00:56 -0300

    Adicionando Allowed Hosts

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9+wv!r8ibll+76d_e6(czci6mypwt-2=4^-^3rb)d1a_j$b$z5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()) #  localhost, 127.0.0.1, pypto-django.herokuapp.com
 
 
 # Application definition
